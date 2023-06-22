bdf1342632692640347f27293aa6eb65658adecf
Adnan Tawaffani <adnan.tawaffani@gmail.com>
2023-06-22 10:13:16 +0700

    Updated

--- test.py
+++ test.py
@@ -25,61 +25,63 @@ if __name__ == '__main__':
         st.write('Pilih gambar yang berbeda menggunakan dropdown.')
 
         image = cv2.imread(filepath)
-    blur_image=cv2.medianBlur(image,7)
+        blur_image=cv2.medianBlur(image,7)
 
-    grey = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
+        grey = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
 
-    thresh = cv2.adaptiveThreshold(grey,200,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,41,25)
+        thresh = cv2.adaptiveThreshold(grey,200,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,41,25)
 
-    contours,hierarchy= cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours,hierarchy= cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    preprocessed_digits = []
+        preprocessed_digits = []
 
-    # initialize the reverse flag and sort index
-    # handle if we need to sort in reverse
-    boundingBoxes = [cv2.boundingRect(c) for c in contours]
-    (contours, boundingBoxes) = zip(*sorted(zip(contours, boundingBoxes),
-                                    key=lambda b:b[1][0], reverse=False))
+        # initialize the reverse flag and sort index
+        # handle if we need to sort in reverse
+        boundingBoxes = [cv2.boundingRect(c) for c in contours]
+        (contours, boundingBoxes) = zip(*sorted(zip(contours, boundingBoxes),
+                                        key=lambda b:b[1][0], reverse=False))
 
 
-    for c in contours:
-        x,y,w,h = cv2.boundingRect(c)
+        for c in contours:
+            x,y,w,h = cv2.boundingRect(c)
 
-        # Creating a rectangle around the digit in the original image (for displaying the digits fetched via contours)
-        cv2.rectangle(blur_image, (x,y), (x+w, y+h), color=(255, 0, 0), thickness=2)
+            # Creating a rectangle around the digit in the original image (for displaying the digits fetched via contours)
+            cv2.rectangle(blur_image, (x,y), (x+w, y+h), color=(255, 0, 0), thickness=2)
 
-        # Cropping out the digit from the image corresponding to the current contours in the for loop
-        digit = thresh[y:y+h, x:x+w]
+            # Cropping out the digit from the image corresponding to the current contours in the for loop
+            digit = thresh[y:y+h, x:x+w]
 
-        # Resizing that digit to (18, 18)
-        resized_digit = cv2.resize(digit, (18,18))
+            # Resizing that digit to (18, 18)
+            resized_digit = cv2.resize(digit, (18,18))
 
-        # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
-        padded_digit = np.pad(resized_digit, ((5,5),(5,5)), "constant", constant_values=0)
+            # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
+            padded_digit = np.pad(resized_digit, ((5,5),(5,5)), "constant", constant_values=0)
 
-        # Adding the preprocessed digit to the list of preprocessed digits
-        preprocessed_digits.append(padded_digit)
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Contoured Image",color='red')
-    plt.imshow(image, cmap="gray")
-    plt.show()
+            # Adding the preprocessed digit to the list of preprocessed digits
+            preprocessed_digits.append(padded_digit)
 
-    inp = np.array(preprocessed_digits)
-    figr=plt.figure(figsize=(len(inp),4))
-    i=1
-    alphabets=[]
-    for digit in preprocessed_digits:
-        [prediction] = model.predict(digit.reshape(1, 28, 28, 1)/255.)
-        pred=alpha[np.argmax(prediction)]
-        alphabets.append(pred)
-        figr.add_subplot(1,len(inp),i)
-        i+=1
         plt.xticks([])
         plt.yticks([])
-        plt.imshow(digit.reshape(28, 28), cmap="gray")
-        plt.title(pred,color='green',fontsize=18,fontweight="bold")
-    st.write("The Recognized Alphabets are : " ,*alphabets)
+        plt.title("Contoured Image",color='red')
+        plt.imshow(image, cmap="gray")
+        plt.show()
+
+        inp = np.array(preprocessed_digits)
+        figr=plt.figure(figsize=(len(inp),4))
+        i=1
+        alphabets=[]
+        for digit in preprocessed_digits:
+            [prediction] = model.predict(digit.reshape(1, 28, 28, 1)/255.)
+            pred=alpha[np.argmax(prediction)]
+            alphabets.append(pred)
+            figr.add_subplot(1,len(inp),i)
+            i+=1
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(digit.reshape(28, 28), cmap="gray")
+            plt.title(pred,color='green',fontsize=18,fontweight="bold")
+            
+        st.write("The Recognized Alphabets are : " ,*alphabets)
 
         # img = image.load_img(filename)
         # x = image.img_to_array(img)
